4844dcf4ef442141bcc44667c6e54aeee235c942
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2023-07-22 01:45:45 +0100

    Addded the rich text to various forms

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,10 @@
 class Application < ApplicationRecord
   validates :applicant_information, :resume_text, :cover_letter, :application_status, presence: true
 
+  has_rich_text :applicant_information
+  has_rich_text :resume_text
+  has_rich_text :cover_letter
+
   belongs_to :user
   belongs_to :job
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,8 @@
 class Job < ApplicationRecord
   validates :title, :description, presence: true
 
+  has_rich_text :description
+
   has_many :applications
   belongs_to :user
 end
