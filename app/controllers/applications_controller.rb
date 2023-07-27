45dfad3483d58833b80e5cb50cbe671bf531b351
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2023-07-27 15:59:43 +0100

    Added logic to authorize job poster and job seekers

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,44 +3,61 @@ class ApplicationsController < ApplicationController
   before_action :authenticate_user!, only: %i[create]
 
   def create
-    @job = Job.find(params[:job_id])
-    @application = @job.applications.new(application_params)
-    @application.submitted_at = Date.today
-    @application.user_id = current_user.id
-    @application.job_id = params[:job_id]
-    if @application.save
-      redirect_to job_path(@job)
+    if current_user.role == 'job seeker'
+      @job = Job.find(params[:job_id])
+      @application = @job.applications.new(application_params)
+      @application.submitted_at = Date.today
+      @application.user_id = current_user.id
+      @application.job_id = params[:job_id]
+      if @application.save
+        redirect_to job_path(@job)
+      else
+        redirect_to :root, status: :unauthorized, notice: 'Something went wrong while creating an application to the job post.'
+      end
     else
-      redirect_to :root, status: :unauthorized, notice: 'Something went wrong while creating an application to the job post.'
+      redirect_to '/', status: :unauthorized, notice: 'You are not authorized to create job applications.'
     end
   end
 
   def edit
+    if current_user.role == 'job seeker'
+      # resume as usual
+    else
+      redirect_to '/', status: :unauthorized, notice: 'You are not authorized to edit a job application.'
+    end
   end
 
   def update
-    @job = Job.find(params[:job_id])
-    @application = Application.find(params[:id])
-      if @application.update_attribute(:application_status, application_status_param["application_status"])
-        if @application.application_status.present?
-          if @application.application_status == 'Accepted'
-            flash[:notice] = "You just accepted an application, congratulations."
-          else
-            flash[:alert] = "You just declined an application."
+    if current_user.role == 'job seeker'
+      @job = Job.find(params[:job_id])
+      @application = Application.find(params[:id])
+        if @application.update_attribute(:application_status, application_status_param["application_status"])
+          if @application.application_status.present?
+            if @application.application_status == 'Accepted'
+              flash[:notice] = "You just accepted an application, congratulations."
+            else
+              flash[:alert] = "You just declined an application."
+            end
           end
+          redirect_to job_path(@job)
+        else
+          redirect_to job_path(@job), status: :unprocessable_entity, notice: 'Unable to update the application status.'
         end
-        redirect_to job_path(@job)
-      else
-        redirect_to job_path(@job), status: :unprocessable_entity, notice: 'Unable to update the application status.'
-      end
+    else
+      redirect_to '/', status: :unauthorized, notice: 'You are not authorized to update a job application.'
+    end
   end
 
   def destroy
-    @application = Application.find(params[:application][:application_id])
-    if @application.destroy
-      flash[:alert] = "You deleted the job application successfully."
+    if current_user.role == 'job seeker'
+      @application = Application.find(params[:application][:application_id])
+      if @application.destroy
+        flash[:alert] = "You deleted the job application successfully."
+      else
+        flash[:notice] = "You did not delete the job application."
+      end
     else
-      flash[:notice] = "You did not delete the job application."
+      redirect_to '/', status: :unauthorized, notice: 'You are not authorized to update a job application.'
     end
   end
 
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -21,70 +21,82 @@ class JobsController < ApplicationController
 
   # GET /jobs/new
   def new
-
-    @job = Job.new
+    if current_user.role == 'job poster'
+      @job = Job.new
+    else
+      redirect_to '/', status: :unauthorized, notice: 'You are not authorized to post jobs.'
+    end
   end
 
   # GET /jobs/1/edit
   def edit
-
-    if Job.where('user_id = ?', current_user.id).count > 5 || @job.user_id != current_user.id
-      @jobs = Job.all
-      return redirect_to '/', status: :unauthorized, notice: 'You are not authorize to post more than 5 job postings.'
+    if current_user.role == 'job poster'
+      if Job.where('user_id = ?', current_user.id).count > 5 || @job.user_id != current_user.id
+        @jobs = Job.all
+        return redirect_to '/', status: :unauthorized, notice: 'You are not authorize to post more than 5 job postings.'
+      end
+    else
+      redirect_to '/', status: :unauthorized, notice: 'You are not authorized to edit jobs.'
     end
-
   end
 
   # POST /jobs or /jobs.json
   def create
-
-    @job = Job.new(job_params)
-
-    @job.user_id = current_user.id
-
-    respond_to do |format|
-      if @job.save
-        format.html { redirect_to job_url(@job), notice: "Job was successfully created." }
-        format.json { render :show, status: :created, location: @job }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
+    if current_user.role == 'job poster'
+      @job = Job.new(job_params)
+      @job.user_id = current_user.id
+      respond_to do |format|
+        if @job.save
+          format.html { redirect_to job_url(@job), notice: "Job was successfully created." }
+          format.json { render :show, status: :created, location: @job }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @job.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to '/', status: :unauthorized, notice: 'You are not authorized to post jobs.'
     end
   end
 
   # PATCH/PUT /jobs/1 or /jobs/1.json
   def update
-
-    if Job.where('user_id = ?', current_user.id).count > 5 || @job.user_id != current_user.id
-      @jobs = Job.all
-      return redirect_to '/', status: :unauthorized, notice: 'You are not authorize to post more than 5 job postings.'
-    end
-
-    respond_to do |format|
-      if @job.update(job_params) && @job.user_id == current_user.id
-        format.html { redirect_to job_url(@job), notice: "Job was successfully updated." }
-        format.json { render :show, status: :ok, location: @job }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
+    if current_user.role == 'job poster'
+      if Job.where('user_id = ?', current_user.id).count > 5 || @job.user_id != current_user.id
+        @jobs = Job.all
+        return redirect_to '/', status: :unauthorized, notice: 'You are not authorize to post more than 5 job postings.'
       end
+
+      respond_to do |format|
+        if @job.update(job_params) && @job.user_id == current_user.id
+          format.html { redirect_to job_url(@job), notice: "Job was successfully updated." }
+          format.json { render :show, status: :ok, location: @job }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @job.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      redirect_to '/', status: :unauthorized, notice: 'You are not authorized to update jobs.'
     end
   end
 
   # DELETE /jobs/1 or /jobs/1.json
   def destroy
+    if current_user.role == 'job poster'
+      if @job.user_id != current_user.id
+        @jobs = Job.all
+        return redirect_to '/', status: :unauthorized, notice: 'You are not authorize to delete this post.'
+      end
 
-    if @job.user_id != current_user.id
-      @jobs = Job.all
-      return redirect_to '/', status: :unauthorized, notice: 'You are not authorize to delete this post.'
-    end
+      @job.destroy
 
-    @job.destroy
-
-    respond_to do |format|
-      format.html { redirect_to jobs_url, notice: "Job was successfully destroyed." }
-      format.json { head :no_content }
+      respond_to do |format|
+        format.html { redirect_to jobs_url, notice: "Job was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to '/', status: :unauthorized, notice: 'You are not authorized to destroy jobs.'
     end
   end
 
